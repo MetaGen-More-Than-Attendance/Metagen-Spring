31773496e16f3bbb7693ec241087fd17b9680522
stoylan <toylansamet@gmail.com>
2022-05-04 18:54:24 +0300

    getAllInstructor api has written

--- src/main/java/com/hst/metagen/service/concretes/InstructorServiceImpl.java
+++ src/main/java/com/hst/metagen/service/concretes/InstructorServiceImpl.java
@@ -2,11 +2,13 @@ package com.hst.metagen.service.concretes;
 
 import com.hst.metagen.entity.Instructor;
 import com.hst.metagen.entity.Role;
+import com.hst.metagen.entity.Student;
 import com.hst.metagen.repository.InstructorRepository;
 import com.hst.metagen.service.abstracts.FileService;
 import com.hst.metagen.service.abstracts.InstructorService;
 import com.hst.metagen.service.abstracts.RoleService;
 import com.hst.metagen.service.dtos.InstructorDto;
+import com.hst.metagen.service.dtos.StudentDto;
 import com.hst.metagen.service.requests.CreateInstructorRequest;
 import com.hst.metagen.util.mapping.ModelMapperService;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -59,5 +62,11 @@ public class InstructorServiceImpl implements InstructorService {
         Instructor instructor = instructorRepository.getById(instructorId);
         Path path = Paths.get(instructor.getPhotoPath());
         return Files.readAllBytes(path);
+    }
+
+    @Override
+    public List<InstructorDto> getAllInstructor() {
+        List<Instructor> instructorList = instructorRepository.findAll();
+        return modelMapperService.entityToDtoList(instructorList, InstructorDto.class);
     }
 }
